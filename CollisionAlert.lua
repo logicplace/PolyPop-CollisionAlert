2e7e13a7716bb690e79e47efc12497c2c0254516
Uriyel <jcollins1106@gmail.com>
2024-01-31 08:18:32 -0500

    instancing and range corrections

--- CollisionAlert.lua
+++ CollisionAlert.lua
@@ -1,6 +1,6 @@
 
 Instance.properties = properties({
-	{ name="Sensitivity", type="Real", value=0, range={min=0, max=3}, onUpdate="onInstanceUpdate" },
+	{ name="Sensitivity", type="Real", value=0, range={min=0.0, max=3.0}, onUpdate="onInstanceUpdate" },
 	{ name="onCollision", type="Alert" }
 })
 
@@ -16,11 +16,11 @@ function Instance:onInit()
 		self.collisionModel = getEditor():createNew(self:getParent():getObjectKit(), "CollisionModel")	
 	end
 
-	icd = getEditor():createNew(self.collisionModel:getCollisionDetectors(), "ImpactCollisionDetector")
-	icd:setName("AlertImpactCollisionDetector")
-	icd.threshold = 3.0
+	self.icd = getEditor():createNew(self.collisionModel:getCollisionDetectors(), "ImpactCollisionDetector")
+	self.icd:setName("AlertImpactCollisionDetector")
+	self.icd.threshold = 3.0
 
-	self.resp = getEditor():createNew(icd:getCollisionResponses(), "ScriptCollisionResponse",  getLocalFolder() .. "CollisionAlertModel.lua")
+	self.resp = getEditor():createNew(self.icd:getCollisionResponses(), "ScriptCollisionResponse",  getLocalFolder() .. "CollisionAlertModel.lua")
 	self.resp:setOnCollision(self.properties.onCollision)
 
 	if (self:getParent():hasEvent("onInstanceUpdate")) then
@@ -32,14 +32,14 @@ end
 function Instance:onInstanceUpdate()
 	if (self:getParent():getInstance():getRigidBody()) then
 		self:getParent():getInstance():getRigidBody().collisionModel = self.collisionModel
-		icd.threshold = Instance.properties.Sensitivity
+		self.icd.threshold = Instance.properties.Sensitivity
 	end
 end
 
 function Instance:onDelete()
 
-	local icd = self.collisionModel:getCollisionDetectors():findObjectByName("AlertImpactCollisionDetector")	
-	getEditor():removeFromLibrary(icd)
+	self.icd = self.collisionModel:getCollisionDetectors():findObjectByName("AlertImpactCollisionDetector")	
+	getEditor():removeFromLibrary(self.icd)
 	if (self.collisionModel:getCollisionDetectors():getObjectCount() == 0) then
 		getEditor():removeFromLibrary(self.collisionModel)
 	end
